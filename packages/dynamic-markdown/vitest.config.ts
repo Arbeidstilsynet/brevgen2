0aa22a9ed1f31d5b61309da04d13f357d311d8b5
Grunde Thorheim <grunde.thorheim@arbeidstilsynet.no>
2024-12-11 13:15:04 +0000

    Merged PR 15681: build: Dep update, fjernet build for dynamic-markdown
    Oppdatert avhengigheter, inkludert React 19 stable
    
    Fjernet Vite og build-script for `packages/dynamic-markdown`. Den blir JIT-compiled av konsumentene. Se https://turbo.build/repo/docs/core-concepts/internal-packages#just-in-time-packages

--- packages/dynamic-markdown/vitest.config.ts
+++ packages/dynamic-markdown/vitest.config.ts
@@ -3,7 +3,5 @@ import { defineConfig } from "vite";
 
 // override vite config to prevent ESlint from running in tests
 export default defineConfig({
-  test: {
-    // ... Specify options here.
-  },
+  test: {},
 });
