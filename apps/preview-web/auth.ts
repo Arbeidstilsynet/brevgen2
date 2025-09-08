32286aae4efa49be5053cfe914152c89362b3bfd
Thorheim, Grunde Andreas Grepstad <grunde.thorheim@arbeidstilsynet.no>
2025-09-08 14:41:13 +0000

    Merged PR 19621: fix(docker): trust host
    Related work items: #34893

--- apps/preview-web/auth.ts
+++ apps/preview-web/auth.ts
@@ -4,6 +4,7 @@ import MicrosoftEntraID from "next-auth/providers/microsoft-entra-id";
 export const tenantId = process.env.AZURE_TENANT_ID ?? "da4bf886-a8a6-450d-a806-c347b8eb8d80";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
+  trustHost: true, // https://authjs.dev/getting-started/deployment#docker
   providers: [
     MicrosoftEntraID({
       clientId: process.env.AZURE_APPLICATION_ID!,
