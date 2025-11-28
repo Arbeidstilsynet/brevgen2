0fb5b3f6ec568d3fc5dc64d7c6d504e9e638a4f0
Uijting, Sverre Francis <sverre.francis.uijting@arbeidstilsynet.no>
2025-11-28 11:16:38 +0000

    Merged PR 20732: feat(web): La til yrkeskvalifikasjoner
    La til yrkeskvalifikasjoner

--- apps/preview-web/components/config/selectableRepos.ts
+++ apps/preview-web/components/config/selectableRepos.ts
@@ -44,6 +44,12 @@ export const allowedRepos: RepoInfo[] = [
     prettyName: "Arbeidstid",
     onlyPaths: ["dokumentmaler"],
   },
+  {
+    id: "a0c5d4c8-d5a3-46f2-9d59-54ecebddb62a",
+    repoName: "yrkeskvalifikasjon-monorepo",
+    prettyName: "Yrkeskvalifikasjon",
+    onlyPaths: ["backend/AutomatiskVedtak/Infrastructure/Adapters/dokumentmaler"],
+  },
 ].toSorted((a, b) => a.prettyName.localeCompare(b.prettyName));
 
 export const allowedRepoNames = new Set(allowedRepos.map((r) => r.repoName));
