f2208953f0cc005d8a2fbda417c471008e3fd17c
Thorheim, Grunde Andreas Grepstad <grunde.thorheim@arbeidstilsynet.no>
2025-12-03 14:10:41 +0000

    Merged PR 20860: fix(web/auth): add image handling in JWT callback to prevent large cookie errors

--- apps/preview-web/auth.ts
+++ apps/preview-web/auth.ts
@@ -2,6 +2,7 @@ import { AuthOptions, getServerSession } from "next-auth";
 import AzureAdProvider from "next-auth/providers/azure-ad";
 
 export const tenantId = process.env.AZURE_TENANT_ID ?? "da4bf886-a8a6-450d-a806-c347b8eb8d80";
+const MAX_PICTURE_SIZE = 4 * 1024;
 
 export const authOptions = {
   providers: [
@@ -15,6 +16,23 @@ export const authOptions = {
     strategy: "jwt",
     maxAge: 8 * 60 * 60,
     updateAge: 30 * 60,
+  },
+  callbacks: {
+    jwt({ token, user, profile }) {
+      if (user && profile) {
+        // Copy a *safe* image value, but strip it if too large
+        // workaround for "400 Request Header Or Cookie Too Large" response from nginx
+        // despite increasing nginx.ingress.kubernetes.io/proxy-buffer-size
+        const candidateImage = user.image ?? profile.image;
+        if (candidateImage && candidateImage.length < MAX_PICTURE_SIZE) {
+          token.picture = candidateImage;
+        } else {
+          token.picture = null;
+        }
+      }
+
+      return token;
+    },
   },
 } as const satisfies AuthOptions;
 
