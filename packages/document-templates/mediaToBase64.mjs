4b7ba60446a400e33e84117b4360e288ca2b6814
Grunde Thorheim <grunde.thorheim@arbeidstilsynet.no>
2026-01-22 11:27:43 +0100

    direktorat logo

--- packages/document-templates/mediaToBase64.mjs
+++ packages/document-templates/mediaToBase64.mjs
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename);
 
 // Resolve the path to the file
 const imagePath = path.resolve(__dirname, argv[2]);
-console.log(imagePath);
+
 const buffer = fs.readFileSync(imagePath);
 const base64Image = buffer.toString("base64");
 const fullBase64Image = `data:image/png;base64,${base64Image}`;
