7ee4b7a922a4c5df4216e3ce364b6bf2ccee6a76
Grunde Thorheim <55745090+vaernion@users.noreply.github.com>
2026-03-26 15:48:09 +0100

    fix(templates/blank): apply earlier css improvements (#320)
    resolves https://github.com/Arbeidstilsynet/team-plattform/issues/115
    resolves https://github.com/Arbeidstilsynet/team-plattform/issues/112

--- packages/document-templates/lib/blank.ts
+++ packages/document-templates/lib/blank.ts
@@ -1,35 +1,4 @@
-// old global CSS before whitespace adjustments, replace with standard globalCss when consumers are ready
-const globalCss = `
-body {
-  font-family: 'Aptos', 'Helvetica Neue', Arial, sans-serif;
-  font-size: 11.5pt;
-  }
-h1 {
-  font-size: 22pt;
-  font-weight: normal;
-}
-h2 {
-  font-size: 16pt;
-  font-weight: normal;
-}
-h3 {
-  font-size: 14pt;
-  font-weight: normal;
-}
-h4 {
-  font-size: 11.5pt;
-  font-weight: bold;
-}
-p {
-  break-inside: avoid;
-}
-a {
-  color: #3C4C7D;
-}
-table {
-  width: 100%;
-}
-`;
+import { globalCss } from "./css";
 
 export const blankTemplate = {
   globalCss,
